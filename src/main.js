24598be2c0cf8c5bf6768d555bfc4803269028c4
Ksu <ksenya-7@yandex.ru>
2022-06-18 13:28:19 +0300

    правит баг статистики

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import {render} from './framework/render.js';
+import {remove, render} from './framework/render.js';
 import StatisticsView from './view/statistics-view.js';
 import FilmsPresenter from './presenter/films-presenter.js';
 import FilterPresenter from './presenter/filter-presenter.js';
@@ -18,13 +18,16 @@ const siteFooterElement = siteBodyElement.querySelector('.footer__statistics');
 const filterModel = new FilterModel();
 const filmsModel = new FilmsModel(new FilmsApiService(END_POINT, AUTHORIZATION));
 const commentsModel = new CommentsModel(new CommentsApiService(END_POINT, AUTHORIZATION));
+const statisticsView = new StatisticsView(filmsModel.films);
 
 const filterPresenter = new FilterPresenter(siteMainElement, filterModel, filmsModel);
 const filmsPresenter = new FilmsPresenter(siteBodyElement, filterModel, filmsModel, commentsModel);
+render(statisticsView, siteFooterElement);
 
 filmsPresenter.init();
 filterPresenter.init();
 filmsModel.init()
   .finally(() => {
+    remove(statisticsView);
     render(new StatisticsView(filmsModel.films), siteFooterElement);
   });
