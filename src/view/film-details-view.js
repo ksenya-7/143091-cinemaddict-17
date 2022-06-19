1e770978333b990ccc4200b7a3752200b543be38
Ksu <ksenya-7@yandex.ru>
2022-06-19 19:41:41 +0300

    экранирует вывод текста

--- src/view/film-details-view.js
+++ src/view/film-details-view.js
@@ -101,7 +101,7 @@ const createFilmPopupTemplate = (film, comments) => {
                   ${comment.emotion ? `<img src="./images/emoji/${comment.emotion}.png" width="55" height="55" alt="emoji-${comment.emotion}">` : ''}
                 </span>
                 <div>
-                  <p class="film-details__comment-text">${comment.comment}</p>
+                  <p class="film-details__comment-text">${`${he.encode(comment.comment)}`}</p>
                   <p class="film-details__comment-info">
                     <span class="film-details__comment-author">${comment.author}</span>
                     <span class="film-details__comment-day">${getHumanizeCommentDate(comment.date)}</span>
